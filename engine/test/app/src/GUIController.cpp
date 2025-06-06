b806a5245533e65ff21b304fac3c10795c7551cd
Mihailo033 <mi22105@alas.matf.bg.ac.rs>
2025-06-06 15:40:02 +0200

    6.6.2025 - 1:
    - Pozicija point svetla se moze menjati dinamicno preko ImGui interfejsa

--- engine/test/app/src/GUIController.cpp
+++ engine/test/app/src/GUIController.cpp
@@ -37,6 +37,9 @@ void GUIController::draw() {
     // Ubaci slider koji menja mainCtrl->m_pointLightIntensity
     // Tasteri: od 0.0 do 5.0 (možeš prilagoditi range)
     ImGui::SliderFloat("Point Light Intensity", &mainCtrl->pointLightIntensity, 0.0f, 5.0f);
+    ImGui::SliderFloat3("Light Position", &mainCtrl->lightPos.x,
+                        -50.0f, 50.0f,
+                        "%.1f");
     ImGui::End();
 
     // Zatvorimo ImGui pass
